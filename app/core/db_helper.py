8743a97c78f9c8c5eb8c048650da462911e5ffd8
melloru <pythonubuntu12@gmail.com>
2025-02-22 15:49:33 -0200

    добавил конфиг и дб хелпер

---
+++ app/core/db_helper.py
@@ -0,0 +1,33 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from core.config import settings
+
+
+class DatabaseHelper:
+    def __init__(
+        self,
+        url: str,
+        echo: bool = False,
+    ):
+        self.engine = create_async_engine(
+            url=url,
+            echo=echo,
+        )
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            expire_on_commit=False,
+            autoflush=False,
+            autocommit=False,
+        )
+
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+
+
+db_helper = DatabaseHelper(
+    url=settings.db.url,
+    echo=settings.db.echo,
+)
