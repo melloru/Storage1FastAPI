8e15cb49f9376901358fc461ef3147c34960385f
melloru <pythonubuntu12@gmail.com>
2025-02-22 13:47:02 -0200

    зависимости и инициализация приложения FastAPI

---
+++ app/main.py
@@ -0,0 +1,13 @@
+import uvicorn
+
+from fastapi import FastAPI
+
+
+main_app = FastAPI()
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:main_app",
+        host="localhost",
+        port=8000,
+    )
